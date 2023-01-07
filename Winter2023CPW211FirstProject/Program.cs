14b87c6e39eb3228748dec48dd1a3997b0de39dd
TUNG N KIM <Tungkims@gmail.com>
2023-01-07 11:41:40 -0800

    added "Thank you!"

--- Winter2023CPW211FirstProject/Program.cs
+++ Winter2023CPW211FirstProject/Program.cs
@@ -4,4 +4,6 @@ Console.WriteLine("This project is my first .NET assignment for CPW211 using Git
 for (int i = 0; i < 25; i++)
 {
     Console.WriteLine($"Number: {i}");
-}    
+}
+
+Console.WriteLine("Thank you for testing out my project!");
