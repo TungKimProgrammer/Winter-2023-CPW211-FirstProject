d6c5ed6d6925208a0baea0464991fa9d2a76c0d9
TUNG N KIM <Tungkims@gmail.com>
2023-01-07 11:39:46 -0800

    added number 1 to 25 display

--- Winter2023CPW211FirstProject/Program.cs
+++ Winter2023CPW211FirstProject/Program.cs
@@ -1,2 +1,7 @@
 ﻿Console.WriteLine("Hello, GitHub!");
 Console.WriteLine("This project is my first .NET assignment for CPW211 using GitHub");
+
+for (int i = 0; i < 25; i++)
+{
+    Console.WriteLine($"Number: {i}");
+}    
